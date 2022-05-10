94c5e32164d66454e0da85f143d72115a086aa51
Allan Queiroz <allan.queiroz.dev@gmail.com>
2022-05-09 21:46:40 -0300

    editted app.js

--- front/src/App.js
+++ front/src/App.js
@@ -1,63 +1,42 @@
 import React from "react";
 import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const schema = yup.object().shape({
+  name: yup.string().required(),
+  age: yup.number().integer().positive(),
+});
+
+const defaultValues = {
+  name: "",
+  age: 0,
+};
 
 const App = () => {
-  const [name, setName] = React.useState("");
-  const [email, setEmail] = React.useState("");
-  const [passwd, setPasswd] = React.useState("");
-
-  const schema = yup.object().shape({
-    name: yup.string().required(),
-    email: yup.string().email().required(),
-    passwd: yup.string().min(4).required("AA"),
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm({
+    defaultValues,
+    resolver: yupResolver(schema),
   });
 
-  const data = React.useMemo(() => {
-    return {
-      name,
-      email,
-      passwd,
-    };
-  }, [name, email, passwd]);
-
-  const submit = (e) => {
-    e.preventDefault();
-    schema.isValid(data).then((valido) => {
-      valido ? alert("AEW, ta tudo certo") : alert("Tá errado carai");
-    });
-  };
-
-  const arrayTest = [['aUmValor', 'testado'], ['novovalor', 99], ['finalValue', 33, 'carro']];
-  const foiConvertido = Object.fromEntries(arrayTest);
-  const novoFoiConvertido = {...foiConvertido}
-  console.log(foiConvertido);
-  console.log('>', novoFoiConvertido);
+  function submit(data) {
+    console.log(JSON.stringify(data));
+  }
 
   return (
     <React.Fragment>
       <h1>Hello World</h1>
-      <form>
-        <input
-          type="text"
-          placeholder="nome"
-          title="insira seu nome"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="e-mail"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="senha"
-          value={passwd}
-          onChange={(e) => setPasswd(e.target.value)}
-        />
+      <form onSubmit={handleSubmit(submit)}>
+        <input {...register("name")} type="text" placeholder="nome" />
+        {errors.name && "First name is required"}
+        <input {...register("age")} type="number" placeholder="idade" />
+        {errors.age && "Age must be positive number"}
         <hr />
-        <button onClick={submit}>GO!</button>
+        <button type="submit">Submeter!</button>
       </form>
     </React.Fragment>
   );
