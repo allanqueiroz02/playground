7c703be1cc42ca8afb3a968aef5da20d6ee8cd0a
Allan Queiroz <allan.queiroz.dev@gmail.com>
2023-01-13 22:40:09 -0300

    ThemeProvider & animation

--- front/myStyledComp/src/App.jsx
+++ front/myStyledComp/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
-import styled from "styled-components";
+import styled, { keyframes, ThemeProvider } from "styled-components";
 
-import { CustomInput } from "./components";
+import { CustomButton, CustomInput } from "./components";
 import Espada from "./icons/espada2.png";
 
 const Container = styled.div`
   padding: 30px;
-  background-color: #0c0f6b;
+  background-color: #272970;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -22,22 +22,52 @@ const FirstTitle = styled.h1`
   font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
 `;
 
+const rotate = keyframes`
+ from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const Rotate = styled.div`
+  display: inline-block;
+  animation: ${rotate} 2s linear infinite;
+  padding: 2rem 1rem;
+  font-size: 1.2rem;
+  color: #80ee80;
+`;
+
+const theme = {
+  primary: {
+    primaryColor: "#059862",
+    seconderyColor: "#fff",
+  },
+};
+
 function App() {
   const [changeColorTitle, setChangeColorTitle] = useState(false);
 
   return (
-    <Container>
-      <FirstTitle variant={changeColorTitle}>
-        Styled Componentes com Vite
-      </FirstTitle>
-      <CustomInput />
-      <input
-        type="checkbox"
-        name="change_color"
-        value={changeColorTitle}
-        onClick={() => setChangeColorTitle((oldV) => !oldV)}
-      />
-    </Container>
+    <ThemeProvider theme={theme}>
+      <Container>
+        <FirstTitle variant={changeColorTitle}>
+          Styled Componentes com Vite
+        </FirstTitle>
+        <CustomInput />
+        <Rotate>Girando</Rotate>
+        <input
+          type="checkbox"
+          name="change_color"
+          value={changeColorTitle}
+          onClick={() => setChangeColorTitle((oldV) => !oldV)}
+        />
+        <CustomButton onClick={() => console.log("Consologzou")}>
+          ENVIAR TUDO CARAIO
+        </CustomButton>
+      </Container>
+    </ThemeProvider>
   );
 }
 
