14d773c814c646b5e2ed4d71052ff316981ee43c
Allan Queiroz <allan.queiroz.dev@gmail.com>
2023-02-15 22:37:51 -0300

    usando ts

---
+++ front/index.js
@@ -0,0 +1,29 @@
+"use strict";
+function calculaArea(tipo, valorA, valorB) {
+    let res = 0;
+    switch (tipo) {
+        case "triangulo":
+            res = (valorA * valorB) / 2;
+            console.log("> Resultado:", res);
+            break;
+        case "retangulo":
+            res = valorA * valorB;
+            console.log("> Resultado:", res);
+            break;
+        case "trapezio":
+            res = ((valorA + valorA) * valorB) / 2;
+            console.log("> Resultado:", res);
+            break;
+        case "circulo":
+            res = 3.14 * (valorA * valorA);
+            console.log("> Resultado:", res);
+            break;
+        default:
+            alert("Digita algo certo ae caraio!!");
+            break;
+    }
+}
+const tipo = prompt("Digite o tipo: ");
+const valorA = prompt("Valor A: ");
+const valorB = prompt("Valor B: ");
+calculaArea(tipo, Number(valorA), Number(valorB));
