3eacc0649b352ed60e5981d03c48429ed0758f99
Allan Queiroz <allan.queiroz.dev@gmail.com>
2023-02-16 08:18:10 -0300

    typescriptando

--- front/index.ts
+++ front/index.ts
@@ -1,37 +1,30 @@
-type TiposAreas =
-  | string
-  | null
-  | "triangulo"
-  | "retangulo"
-  | "trapezio"
-  | "circulo";
-function calculaArea(tipo: TiposAreas, valorA: number, valorB: number) {
-  let res: number = 0;
-  switch (tipo) {
-    case "triangulo":
-      res = (valorA * valorB) / 2;
-      console.log("> Resultado:", res);
-      break;
-    case "retangulo":
-      res = valorA * valorB;
-      console.log("> Resultado:", res);
-      break;
-    case "trapezio":
-      res = ((valorA + valorA) * valorB) / 2;
-      console.log("> Resultado:", res);
-      break;
-    case "circulo":
-      res = 3.14 * (valorA * valorA);
-      console.log("> Resultado:", res);
-      break;
-    default:
-      alert("Digita algo certo ae caraio!!");
-      break;
-  }
+interface InfoFabricante {
+  nomeEmpresa: string;
+  cnpjEmpresa: number;
 }
 
-const tipo = prompt("Digite o tipo: ");
-const valorA = prompt("Valor A: ");
-const valorB = prompt("Valor B: ");
+interface Produto {
+  nome: string;
+  garantia: number;
+  qtdDisponivel?: number;
+  fabricante: InfoFabricante;
+}
 
-calculaArea(tipo, Number(valorA), Number(valorB));
+function showProduct(produto: Produto): void {
+  console.log(`
+    Produto: ${produto.nome}, feito pela ${produto.fabricante.nomeEmpresa}, possui garantia de ${produto.garantia} anos.
+    Possui em estoque ${produto.qtdDisponivel} unidades.
+  `);
+}
+
+const notebook: Produto = {
+  nome: "legion 5i",
+  garantia: 2,
+  qtdDisponivel: 22,
+  fabricante: {
+    nomeEmpresa: "Lenovo",
+    cnpjEmpresa: 1532180001,
+  },
+};
+
+showProduct(notebook);
