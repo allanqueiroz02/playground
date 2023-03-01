29df55d7b2f70ef81cfeac9870ec1f3c9e09d684
Allan Queiroz <allan.queiroz.dev@gmail.com>
2023-02-28 22:52:35 -0300

    ts neles

--- front/index.js
+++ front/index.js
@@ -6,8 +6,45 @@ function greeting(firstName, lastName) {
 }
 console.log(greeting("Carlão", "Delas"));
 console.log(greeting("Josias"));
+// 0-1
 const t = "MARIA";
 function showNameInLower(name) {
     return name.toLowerCase();
 }
 console.log(showNameInLower(t));
+// 2.
+function mostraNome(nome, sobrenome) {
+    return `${nome} ${sobrenome}`;
+}
+function saudacoesPara(fn, nomeFn, sobrenFn) {
+    console.log("Olá, td bem?");
+    console.log(fn(nomeFn, sobrenFn));
+}
+saudacoesPara(mostraNome, "Tomas", "Turbando");
+//Generic functions
+function mergeObj(obj1, obj2) {
+    return Object.assign(Object.assign({}, obj1), obj2);
+}
+const obj1 = {
+    n1: 1,
+    n2: 2,
+};
+const obj2 = {
+    n6: 6,
+    n7: 7,
+};
+console.log("MERGED", mergeObj(obj1, obj2));
+function getPosition(arr, index) {
+    return arr[index];
+}
+console.log("Item =", getPosition([1, 2, 3], 2));
+// 4
+function verificaMaior(v1, v2) {
+    let maior;
+    if (+v1 > +v2)
+        maior = v1;
+    else
+        maior = v2;
+    return maior;
+}
+console.log("MAIORAL", verificaMaior("12312", "33"));
--- front/index.ts
+++ front/index.ts
@@ -6,6 +6,8 @@ function greeting(firstName: string, lastName?: string) {
 console.log(greeting("Carlão", "Delas"));
 console.log(greeting("Josias"));
 
+// 0-1
+
 const t = "MARIA";
 
 function showNameInLower(name?: string | undefined) {
@@ -13,3 +15,56 @@ function showNameInLower(name?: string | undefined) {
 }
 
 console.log(showNameInLower(t));
+
+// 2.
+
+function mostraNome(nome: string, sobrenome: string) {
+  return `${nome} ${sobrenome}`;
+}
+
+function saudacoesPara(
+  fn: (nm: string, sn: string) => string,
+  nomeFn: string,
+  sobrenFn: string
+) {
+  console.log("Olá, td bem?");
+  console.log(fn(nomeFn, sobrenFn));
+}
+
+saudacoesPara(mostraNome, "Tomas", "Turbando");
+
+//Generic functions
+
+function mergeObj<T, U>(obj1: T, obj2: U) {
+  return {
+    ...obj1,
+    ...obj2,
+  };
+}
+const obj1 = {
+  n1: 1,
+  n2: 2,
+};
+const obj2 = {
+  n6: 6,
+  n7: 7,
+};
+console.log("MERGED", mergeObj(obj1, obj2));
+
+function getPosition<T>(arr: T[], index: number) {
+  return arr[index];
+}
+
+console.log("Item =", getPosition([1, 2, 3], 2));
+
+// 4
+
+function verificaMaior<T extends number | string>(v1: T, v2: T): T {
+  let maior: T;
+  if (+v1 > +v2) maior = v1;
+  else maior = v2;
+
+  return maior;
+}
+
+console.log("MAIORAL", verificaMaior("12312", "33"));
